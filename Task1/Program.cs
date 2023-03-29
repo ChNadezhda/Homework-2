3b379440db25f4fd7b718a7def3958d449f43e0a
Nadezhda Chumachenko <chnadezhda22@gmail.com>
2023-03-30 00:45:50 +0300

    Task1 added

---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿//Напишите программу, которая принимает на вход 
+//трехзначное число и на выходе показывает
+//вторую цифру этого числа
+// 456 => 5
+// 782 => 8
+// 918 => 1
+
+Main();
+
+void Main() 
+{
+    bool Work = true;
+    while(Work)
+    {
+        Console.WriteLine("Для начала работы введите - start. Для окончания - stop");
+        string a = Console.ReadLine();
+        switch (a)
+        {
+            case "start": Task(); break;
+            case "stop": Work = false; break;
+        }
+    }
+}
+
+void Task()
+{
+    Console.WriteLine("Введите трехзначное число: ");
+    int number = int.Parse(Console.ReadLine());
+
+    int digit = SecondDigit(number);
+    Console.WriteLine($"В числе {number} вторая цифра равна - {digit}");
+}
+
+int SecondDigit(int number) 
+{
+    int a = number / 10;
+    int b = a % 10;
+    
+    return b;
+}
